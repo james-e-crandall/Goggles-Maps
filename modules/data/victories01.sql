919d2bff469a9a6e4c4f57539dc0bfb599e5e18a
james-e-crandall <james.e.crandall@gmail.com>
2026-03-01 21:22:29 -0600

    AgeProgressionMilestones

---
+++ modules/data/victories01.sql
@@ -0,0 +1,33 @@
+-- UPDATE AgeProgressions
+-- SET MaxPoints_Abbreviated = MaxPoints_Abbreviated * 2;
+
+-- UPDATE AgeProgressions
+-- SET MaxPoints_Standard = MaxPoints_Standard * 2;
+
+-- UPDATE AgeProgressions
+-- SET MaxPoints_Long = MaxPoints_Long * 2;
+
+-- UPDATE RequirementArguments
+-- SET Value = Value * 2;
+
+
+-- AGE_PROGRESSION_PLAYER_MILESTONE_1
+-- AGE_PROGRESSION_PLAYER_MILESTONE_2
+-- AGE_PROGRESSION_PLAYER_MILESTONE_3
+
+
+UPDATE AgeProgressionMilestones
+SET RequiredPathPoints = 7
+WHERE AgeProgressionEventType = 'AGE_PROGRESSION_PLAYER_MILESTONE_1'
+and LegacyPathType='LEGACY_PATH_ANTIQUITY_MILITARY';
+
+UPDATE AgeProgressionMilestones
+SET RequiredPathPoints = 11
+WHERE AgeProgressionEventType = 'AGE_PROGRESSION_PLAYER_MILESTONE_2'
+and LegacyPathType='LEGACY_PATH_ANTIQUITY_MILITARY';
+
+UPDATE AgeProgressionMilestones
+SET RequiredPathPoints = 15
+WHERE AgeProgressionEventType = 'AGE_PROGRESSION_PLAYER_MILESTONE_3'
+and LegacyPathType='LEGACY_PATH_ANTIQUITY_MILITARY';
+
---
+++ modules/data/victories02.sql
@@ -0,0 +1,33 @@
+-- UPDATE AgeProgressions
+-- SET MaxPoints_Abbreviated = MaxPoints_Abbreviated * 2;
+
+-- UPDATE AgeProgressions
+-- SET MaxPoints_Standard = MaxPoints_Standard * 2;
+
+-- UPDATE AgeProgressions
+-- SET MaxPoints_Long = MaxPoints_Long * 2;
+
+-- UPDATE RequirementArguments
+-- SET Value = Value * 2;
+
+
+-- AGE_PROGRESSION_PLAYER_MILESTONE_1
+-- AGE_PROGRESSION_PLAYER_MILESTONE_2
+-- AGE_PROGRESSION_PLAYER_MILESTONE_3
+
+
+UPDATE AgeProgressionMilestones
+SET RequiredPathPoints = 8
+WHERE AgeProgressionEventType = 'AGE_PROGRESSION_PLAYER_MILESTONE_1'
+and LegacyPathType='LEGACY_PATH_ANTIQUITY_MILITARY';
+
+UPDATE AgeProgressionMilestones
+SET RequiredPathPoints = 12
+WHERE AgeProgressionEventType = 'AGE_PROGRESSION_PLAYER_MILESTONE_2'
+and LegacyPathType='LEGACY_PATH_ANTIQUITY_MILITARY';
+
+UPDATE AgeProgressionMilestones
+SET RequiredPathPoints = 16
+WHERE AgeProgressionEventType = 'AGE_PROGRESSION_PLAYER_MILESTONE_3'
+and LegacyPathType='LEGACY_PATH_ANTIQUITY_MILITARY';
+
