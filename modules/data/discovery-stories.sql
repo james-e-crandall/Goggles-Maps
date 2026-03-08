549ab645fc7206af6d3289f6a363d13a8b0dce66
james-e-crandall <james.e.crandall@gmail.com>
2026-03-07 20:13:49 -0600

    discoveries

--- modules/data/discovery-stories.sql
+++ modules/data/discovery-stories.sql
@@ -1,7 +1,3 @@
-UPDATE NarrativeStories
-SET AllowDuplicates = 1
-WHERE "Queue" LIKE '%NARRATIVE_GROUP_DISCOVER%';
-
 CREATE TABLE TempNarrativeStories AS
 SELECT *
 FROM NarrativeStories
@@ -12,18 +8,18 @@ INSERT INTO Types (Type, Kind)
 SELECT TempNarrativeStories.NarrativeStoryType || "_1_JEC", "KIND_NARRATIVE_STORY"
 FROM TempNarrativeStories;
 
-INSERT INTO Types (Type, Kind)
-SELECT TempNarrativeStories.NarrativeStoryType || "_2_JEC", "KIND_NARRATIVE_STORY"
-FROM TempNarrativeStories;
+-- INSERT INTO Types (Type, Kind)
+-- SELECT TempNarrativeStories.NarrativeStoryType || "_2_JEC", "KIND_NARRATIVE_STORY"
+-- FROM TempNarrativeStories;
 
 -- NarrativeStoryOverrides
 INSERT INTO NarrativeStoryOverrides (NarrativeStoryType, OverriddenStoryType)
 SELECT TempNarrativeStories.NarrativeStoryType || "_1_JEC", "DISCOVERY_BASE"
 FROM TempNarrativeStories;
 
-INSERT INTO NarrativeStoryOverrides (NarrativeStoryType, OverriddenStoryType)
-SELECT TempNarrativeStories.NarrativeStoryType || "_2_JEC", "DISCOVERY_BASE"
-FROM TempNarrativeStories;
+-- INSERT INTO NarrativeStoryOverrides (NarrativeStoryType, OverriddenStoryType)
+-- SELECT TempNarrativeStories.NarrativeStoryType || "_2_JEC", "DISCOVERY_BASE"
+-- FROM TempNarrativeStories;
 
 -- NarrativeStory_Links
 INSERT INTO NarrativeStory_Links (FromNarrativeStoryType, ToNarrativeStoryType, Priority, Name, Description)
@@ -32,11 +28,11 @@ FROM TempNarrativeStories
 INNER JOIN NarrativeStory_Links 
 ON TempNarrativeStories.NarrativeStoryType = NarrativeStory_Links.FromNarrativeStoryType;
 
-INSERT INTO NarrativeStory_Links (FromNarrativeStoryType, ToNarrativeStoryType, Priority, Name, Description)
-SELECT TempNarrativeStories.NarrativeStoryType || "_2_JEC", NarrativeStory_Links.ToNarrativeStoryType, NarrativeStory_Links.Priority, NarrativeStory_Links.Name, NarrativeStory_Links.Description
-FROM TempNarrativeStories
-INNER JOIN NarrativeStory_Links 
-ON TempNarrativeStories.NarrativeStoryType = NarrativeStory_Links.FromNarrativeStoryType;
+-- INSERT INTO NarrativeStory_Links (FromNarrativeStoryType, ToNarrativeStoryType, Priority, Name, Description)
+-- SELECT TempNarrativeStories.NarrativeStoryType || "_2_JEC", NarrativeStory_Links.ToNarrativeStoryType, NarrativeStory_Links.Priority, NarrativeStory_Links.Name, NarrativeStory_Links.Description
+-- FROM TempNarrativeStories
+-- INNER JOIN NarrativeStory_Links 
+-- ON TempNarrativeStories.NarrativeStoryType = NarrativeStory_Links.FromNarrativeStoryType;
 
 -- NarrativeStories
 INSERT INTO NarrativeStories (NarrativeStoryType, 
@@ -67,33 +63,33 @@ TempNarrativeStories.ForceChoice,
 TempNarrativeStories.Activation
 FROM TempNarrativeStories;
 
-INSERT INTO NarrativeStories (NarrativeStoryType, 
-Name,
-Description,
-Completion,
-StoryTitle,
-Age,
-RequirementSetId,
-StartEveryone,
-FirstOnly,
-UIActivation,
-Queue,
-ForceChoice,
-Activation)
-SELECT TempNarrativeStories.NarrativeStoryType || "_2_JEC", 
-TempNarrativeStories.Name,
-TempNarrativeStories.Description,
-TempNarrativeStories.Completion,
-TempNarrativeStories.StoryTitle,
-TempNarrativeStories.Age,
-TempNarrativeStories.RequirementSetId,
-TempNarrativeStories.StartEveryone,
-TempNarrativeStories.FirstOnly,
-TempNarrativeStories.UIActivation,
-TempNarrativeStories.Queue,
-TempNarrativeStories.ForceChoice,
-TempNarrativeStories.Activation
-FROM TempNarrativeStories;
+-- INSERT INTO NarrativeStories (NarrativeStoryType, 
+-- Name,
+-- Description,
+-- Completion,
+-- StoryTitle,
+-- Age,
+-- RequirementSetId,
+-- StartEveryone,
+-- FirstOnly,
+-- UIActivation,
+-- Queue,
+-- ForceChoice,
+-- Activation)
+-- SELECT TempNarrativeStories.NarrativeStoryType || "_2_JEC", 
+-- TempNarrativeStories.Name,
+-- TempNarrativeStories.Description,
+-- TempNarrativeStories.Completion,
+-- TempNarrativeStories.StoryTitle,
+-- TempNarrativeStories.Age,
+-- TempNarrativeStories.RequirementSetId,
+-- TempNarrativeStories.StartEveryone,
+-- TempNarrativeStories.FirstOnly,
+-- TempNarrativeStories.UIActivation,
+-- TempNarrativeStories.Queue,
+-- TempNarrativeStories.ForceChoice,
+-- TempNarrativeStories.Activation
+-- FROM TempNarrativeStories;
 
 DROP TABLE TempNarrativeStories;
 
